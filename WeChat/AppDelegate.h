ba0e872e2d10b0bc90763af4b8ded5f765b4c475
wuqihan <wuqihan123@163.com>
2015-12-29 13:08:16 +0800

    初始化项目 并导入XMPP框架

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by wuqh on 15/12/29.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by wuqh on 15/12/29.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
